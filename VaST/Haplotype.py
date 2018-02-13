fab1db633b9e8b3cdaf05b674cc9042b76b0dbc4
Tara Furstenau <tarafurstenau@gmail.com>
2018-02-13 11:08:12 -0700

    fix get flags

--- VaST/Haplotype.py
+++ VaST/Haplotype.py
@@ -41,9 +41,9 @@ class Haplotype:
                     stop + primer_zone_size < genome_size
                     else genome_size - 1)
                 upstream_flags = np.array(
-                    flag_df[flag_df.genome == genome].iloc[up_start: up_stop].Flag, dtype=bool)
+                    flag_df[flag_df.Genome == genome].iloc[up_start: up_stop].Flag, dtype=bool)
                 downstream_flags = np.array(
-                    flag_df[flag_df.genome == genome].iloc[down_start: down_stop].Flag, dtype=bool)
+                    flag_df[flag_df.Genome == genome].iloc[down_start: down_stop].Flag, dtype=bool)
                 upstream_count = np.array([sum(1 for _ in g[1])
                     for g in it.groupby(
                         upstream_flags) if np.all(g[0])], dtype=int)
